40bdc2783725dd0ee4ace9749e749fa0bf6be2b3
Eugene Wong <eugene.wong@razerzone.com>
2016-06-02 17:45:49 +0800

    Chapter 8 completed

--- image.go
+++ image.go
@@ -8,6 +8,9 @@ import (
 	"os"
 	"path/filepath"
 	"time"
+	"runtime"
+	"github.com/disintegration/imaging"
+	"image"
 )
 
 const imageIDLength = 10
@@ -27,6 +30,11 @@ type Image struct {
 	Size        int64
 	CreatedAt   time.Time
 	Description string
+}
+
+func init() {
+	// Ensure out goroutines run across all cores
+	runtime.GOMAXPROCS(runtime.NumCPU())
 }
 
 func NewImage(user *User) *Image {
@@ -90,6 +98,12 @@ func (image *Image) CreateFromURL(imageURL string) error {
 	// The returned value from io.Copy is the number of bytes copied
 	image.Size = size
 
+	// Create the variuos resizes of the images
+	err = image.CreateResizedImages()
+	if err != nil {
+		return err
+	}
+
 	// Save our image to the store
 	return globalImageStore.Save(image)
 }
@@ -119,7 +133,14 @@ func (image *Image) CreateFromFile(file multipart.File, headers *multipart.FileH
 	if err != nil {
 		return err
 	}
+
 	image.Size = size
+
+	// Create the variuos resizes of the images
+	err = image.CreateResizedImages()
+	if err != nil {
+		return err
+	}
 
 	// Save the image to the database
 	return globalImageStore.Save(image)
@@ -131,4 +152,58 @@ func (image *Image) StaticRoute() string {
 
 func (image *Image) ShowRoute() string {
 	return "/image/" + image.ID
+}
+
+func (image *Image) CreateResizedImages() error {
+	// Generate an image from file
+	srcImage, err := imaging.Open("./data/images/" + image.Location)
+	if err != nil {
+		return err
+	}
+
+	// Create a channel to receive errors on
+	errorChan := make(chan error)
+
+	// Procress each size
+	go image.resizePreview(errorChan, srcImage)
+	go image.resizeThumbnail(errorChan, srcImage)
+
+	// Wait for images to finish resizing
+	for i := 0; i < 2; i++ {
+		err := <-errorChan
+		if err == nil {
+			return err
+		}
+	}
+	return nil
+}
+
+var widthThumbnail = 400
+
+func (image *Image) resizeThumbnail(errorChan chan error, srcImage image.Image) {
+	dstImage := imaging.Thumbnail(srcImage, widthThumbnail, widthThumbnail, imaging.Lanczos)
+
+	destination := "./data/images/thumbnail/" + image.Location
+	errorChan <- imaging.Save(dstImage, destination)
+}
+
+var widthPreview = 800
+
+func (image *Image) resizePreview(errorChan chan error, srcImage image.Image){
+	size := srcImage.Bounds().Size()
+	ratio := float64(size.Y) / float64(size.X)
+	targetHeight := int(float64(widthPreview) * ratio)
+
+	dstImage := imaging.Resize(srcImage, widthPreview, targetHeight, imaging.Lanczos)
+
+	destination := "./data/images/preview/" + image.Location
+	errorChan <- imaging.Save(dstImage, destination)
+}
+
+func (image *Image) StaticThumbnailRoute() string {
+	return "/im/thumbnail/" + image.Location
+}
+
+func (image *Image) StaticPreviewRoute() string {
+	return "/im/thumbnail/" + image.Location
 }
